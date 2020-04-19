47ca296dc5164ecfc47ea4ee96254c95c89a6c62
kristenv6 <kristenviola6@gmail.com>
2020-04-18 20:59:08 -0500

    Portal scripts for all scenes

---
+++ Assets/TeleportToIsland.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TeleportToIsland : MonoBehaviour
+{
+    public bool triggered;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        triggered = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        triggered = true;
+        SceneManager.LoadScene("IslandScene");
+    }
+}
