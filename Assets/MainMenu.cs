36e202dcfdb5f658e08769bf5e3df307e7303edd
Julia Fiorino <juliaf2@illinois.edu>
2020-04-27 15:13:44 -0500

    Added Main Menu minus being clickable

---
+++ Assets/MainMenu.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniyEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayGame() 
+    {
+        SceneManager.LoadScene("IslandScene");
+    }
+
+    public void QuitGame() 
+    {
+        #if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+        #else
+            Application.Quit();
+        #endif
+    }
+}
