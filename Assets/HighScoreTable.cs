5c803914a29daa29da0625dbe0ae851b8d7db0b6
Julia Fiorino <juliaf2@illinois.edu>
2020-05-06 21:12:04 -0500

    reloading scores each time

--- Assets/HighScoreTable.cs
+++ Assets/HighScoreTable.cs
@@ -17,23 +17,46 @@ public class HighScoreTable : MonoBehaviour
 
         entryTemplate.gameObject.SetActive(false);
 
-        highScoreEntryList = new List<HighScoreEntry>() {
-            new HighScoreEntry{ score = 12345, name = "sarah" },
-            new HighScoreEntry{ score = 32434, name = "jon" },
-            new HighScoreEntry{ score = 10101, name = "jake" },
-            new HighScoreEntry{ score = 39393, name = "jack" },
-            new HighScoreEntry{ score = 29292, name = "sandy" },
-            new HighScoreEntry{ score = 43434, name = "jacob" },
-            new HighScoreEntry{ score = 58585, name = "beatrice" },
-            new HighScoreEntry{ score = 29292, name = "lee" },
-            new HighScoreEntry{ score = 55455, name = "kate" },
-            new HighScoreEntry{ score = 11111, name = "julia" },
-        };
+        AddHighScoreEntry(10000, "Jessica");
+
+        // highScoreEntryList = new List<HighScoreEntry>() {
+        //     new HighScoreEntry{ score = 12345, name = "sarah" },
+        //     new HighScoreEntry{ score = 32434, name = "jon" },
+        //     new HighScoreEntry{ score = 10101, name = "jake" },
+        //     new HighScoreEntry{ score = 39393, name = "jack" },
+        //     new HighScoreEntry{ score = 29292, name = "sandy" },
+        //     new HighScoreEntry{ score = 43434, name = "jacob" },
+        //     new HighScoreEntry{ score = 58585, name = "beatrice" },
+        //     new HighScoreEntry{ score = 29292, name = "lee" },
+        //     new HighScoreEntry{ score = 55455, name = "kate" },
+        //     new HighScoreEntry{ score = 11111, name = "julia" },
+        // };
+
+        string jsonString = PlayerPrefs.GetString("highScoreTable");
+        HighScores highScores = JsonUtility.FromJson<HighScores>(jsonString);
+
+        for (int i = 0; i < highScores.highScoreEntryList.Count; i++) {
+            for (int j = i + 1; j < highScores.highScoreEntryList.Count; j++) {
+                if (highScores.highScoreEntryList[j].score > highScores.highScoreEntryList[i].score) {
+                    // swap
+                    HighScoreEntry temp = highScores.highScoreEntryList[i];
+                    highScores.highScoreEntryList[i] = highScores.highScoreEntryList[j];
+                    highScores.highScoreEntryList[j] = temp;
+                }
+            }
+        }
 
         highScoreEntryTransformList = new List<Transform>();
-        foreach (HighScoreEntry highScoreEntry in highScoreEntryList) {
+        foreach (HighScoreEntry highScoreEntry in highScores.highScoreEntryList) {
             CreateHighScoreEntryTransform(highScoreEntry, entryContainer, highScoreEntryTransformList);
         }
+
+        // HighScores highScores = new HighScores{ highScoreEntryList = highScoreEntryList };
+        // string json = JsonUtility.ToJson(highScores);
+
+        // PlayerPrefs.SetString("highScoreTable", json);
+        // PlayerPrefs.Save();
+        // Debug.Log(PlayerPrefs.GetString("highScoreTable"));
     }
 
     private void CreateHighScoreEntryTransform(HighScoreEntry highScoreEntry, Transform container, List<Transform> transformList) {
@@ -58,7 +81,25 @@ public class HighScoreTable : MonoBehaviour
         transformList.Add(entryTransform);
     }
 
+    private void AddHighScoreEntry(int score, string name) {
+        HighScoreEntry HighScoreEntry = new HighScoreEntry(score, name);
+
+        string jsonString = PlayerPrefs.GetString("highScoreTable");
+        HighScores highScores = JsonUtility.FromJson<HighScores>(jsonString);
+
+        highScores.highScoreEntryList.Add(highScoreEntry);
+
+        string json = JsonUtility.ToJson(highScores);
+        PlayerPrefs.SetString("highScoreTable", json);
+        PlayerPrefs.Save();
+    }
+
+    private class HighScores {
+        public List<HighScoreEntry> highScoreEntryList;
+    }
+
     // a single high score entry
+    [System.Serializable]
     private class HighScoreEntry {
         public int score;
         public string name;
